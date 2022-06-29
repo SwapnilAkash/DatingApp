9de7ac457403bfdfb0b3755fcacba41a8c9029fe
Swapnil <swapnil.akash21@gmail.com>
2022-06-30 02:38:11 +0530

    Added routing and bootstrap theme

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private accountService:AccountService) { }
+  constructor(private accountService:AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -22,7 +23,10 @@ export class RegisterComponent implements OnInit {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => {
+        console.log(error);
+        this.toastr.error(error.error);
+      }
     })
   }
 
