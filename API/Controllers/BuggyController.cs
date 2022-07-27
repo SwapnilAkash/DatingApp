aeb0864aee2996c600e5dcd6bd2ad451d174392d
Swapnil <swapnil.akash21@gmail.com>
2022-07-27 23:40:55 +0530

    Added Error Handling Components

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController:BaseApiController
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret(){
+            return "secret text";
+        }
+        
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound(){
+            var result = _context.Users.Find(-1);
+
+            if(result == null)
+                return NotFound();
+            
+            return Ok(result);
+
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetSereverError(){
+            var result = _context.Users.Find(-1);
+
+            var response = result.ToString();
+
+            return response;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest(){
+            return BadRequest("Not a good Request!!");
+        }
+   }
+}
---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,76 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  templateUrl: './test-errors.component.html',
+  styleUrls: ['./test-errors.component.css']
+})
+export class TestErrorsComponent implements OnInit {
+
+  baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error(){
+    this.httpClient.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  }
+
+  get500Error(){
+    this.httpClient.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  }
+
+  get400Error(){
+    this.httpClient.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  }
+
+  get401Error(){
+    this.httpClient.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  }
+
+  get400ValidationError(){
+    this.httpClient.post(this.baseUrl + 'account/register',{}).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        this.validationErrors = error;
+        console.log(error);
+      }
+    });
+  }
+
+
+}
